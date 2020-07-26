f37e8cefa791923290ad179fde9bec59a87a661b
Elkana Orbach <elkana.orbach@au10tix.com>
2020-07-26 15:46:52 +0300

    Initial Commit

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Elkana Orbach on 26/07/2020.
+//  Copyright © 2020 Elkana Orbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
