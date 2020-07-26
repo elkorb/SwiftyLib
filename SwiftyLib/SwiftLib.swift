1e87d57456b62e181565989a82874f3c6d5d454a
Elkana Orbach <elkana.orbach@au10tix.com>
2020-07-26 15:57:02 +0300

    Add tests

---
+++ SwiftyLib/SwiftLib.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftLib.swift
+//  SwiftyLib
+//
+//  Created by Elkana Orbach on 26/07/2020.
+//  Copyright © 2020 Elkana Orbach. All rights reserved.
+//
+
+import Foundation
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
